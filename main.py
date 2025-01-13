280b42ba23f5a88ec910e3ff7c3bd96703b29cf2
versbyte <medtaha.unity@gmail.com>
2025-01-13 16:13:42 +0100

    separated testing script

--- main.py
+++ main.py
@@ -165,13 +165,3 @@ def McabeThiele(xa, xd, xb, R, q, Pa, Pb, nm):
   plt.show()
 
 
-# Example usage
-xf = 0.458 # Feed composition
-xd = 0.965 # Distillate composition
-xb = 0.011  # Bottoms composition
-R = 1.4  # Reflux ratio
-q = 1 # Feed thermal condition
-Pa = 95 # Saturation vapor pressure of the most volatile component (mmHg)
-Pb = 23.76 # Saturation vapor pressure of the less volatile component (mmHg)
-nm = 0.75 #murphy efficieny (60-70)%
-McabeThiele(xf, xd, xb, R, q, Pa, Pb, nm)
