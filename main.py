4c9445bad0dad10ca1c59bddef32462b048ff47d
versbyte <medtaha.unity@gmail.com>
2025-01-13 11:57:48 +0100

    Further explanations, Fixed bugs

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
+r"""
+ \ \    / /          | |         | |      
+  \ \  / /__ _ __ ___| |__  _   _| |_ ___ 
+   \ \/ / _ \ '__/ __| '_ \| | | | __/ _ \
+    \  /  __/ |  \__ \ |_) | |_| | ||  __/
+     \/ \___|_|  |___/_.__/ \__, |\__\___|
+                             __/ |        
+                            |___/  
+"""
+
 x = np.linspace(0, 1, 100000)
 
 def courbeEQ(Pa, Pb, nm):
     """
-    This function draws the x-y equilibrium diagram of the most volatile component based
-    on the their saturation vapor pressure.
-    Pa = Saturation vapor pressure of the most volatile component
-    Pb = Saturation vapor pressure of the less volatile component
+    This function calculates and plots the equilibrium curve for the binary mixture based on the saturation vapor pressures of the two components and the Murphy efficiency.
+    Parameters:
+      Pa: Saturation vapor pressure of the more volatile component.
+      Pb: Saturation vapor pressure of the less volatile component.
+      nm: Murphy efficiency (a correction factor for non-ideal behavior).
     """
     global yeq
     y1 = (Pa*x)/(x*Pa + (1-x)*Pb) #y = f(x)
@@ -19,6 +30,15 @@ def courbeEQ(Pa, Pb, nm):
 
 #Operating Line of the Rectifying section
 def DOZE(xa, xd, R, q):
+  """
+  This function calculates and plots the operating line for the rectifying section of the distillation column.
+  It also calculates the intersection point between the rectifying operating line and the q-line.
+  Parameters:
+    xa: Mole fraction of the more volatile component in the feed.
+    xd: Mole fraction of the more volatile component in the distillate.
+    R: Reflux ratio (ratio of liquid returned to the column to the distillate removed).
+    q: Thermal condition of the feed (1 for saturated liquid, 0 for saturated vapor, and values in between for mixed phases).
+  """
   global xdoze, ydoze, Ydoze
   Ydoze = (R / (R + 1)) * x + (xd / (R + 1))
   xdoze = -((xd/(R+1)) + (xa/(q-1)))/((R/(R+1))-(q/(q-1)))
@@ -28,6 +48,15 @@ def DOZE(xa, xd, R, q):
 
 #Operating Line of the Stripping section
 def DOZA(xa, xd, xb, R, q):
+  """
+  This function calculates and plots the operating line for the stripping section of the distillation column.
+  Parameters:
+      xa: Mole fraction of the more volatile component in the feed.
+      xd: Mole fraction of the more volatile component in the distillate.
+      xb: Mole fraction of the more volatile component in the bottoms.
+      R: Reflux ratio.
+      q: Thermal condition of the feed.
+  """
   global Ydoza
   Ydoza = ((ydoze - xb) / (xdoze - xb)) * x + (ydoze - ((ydoze - xb)/(xdoze - xb))*xdoze)
   plt.plot([xb, xb],[0, xb], color = 'k',  linestyle = '--')
@@ -35,6 +64,9 @@ def DOZA(xa, xd, xb, R, q):
 
 #Q-line
 def DA(xa, xd, R, q):
+  """
+  This function calculates and plots the q-line (feed line) and determines the minimum reflux ratio (Rmin).
+  """
   global Yda
   Yda = (q / (q - 1)) * x - (xa / (q - 1))
   xda = ((-xd/(R+1)) - (xa/(q-1)))/((R/(R+1))-(q/(q-1)))
@@ -53,6 +85,10 @@ def DA(xa, xd, R, q):
 
 #Plotting and Calculating the number of theoritical trays
 def theoTrays(xd, xb):
+    """
+    This function calculates and plots the number of theoretical trays required for the separation.
+    It uses interpolation to move between the equilibrium curve and the operating lines (rectifying and stripping) to step off the trays.
+    """
     #interpolations
     feq = interp1d(yeq, x, kind = 'linear')
     fdoze = interp1d(x, Ydoze, kind = 'linear')
@@ -90,6 +126,25 @@ def theoTrays(xd, xb):
 
 #Mcabe-Thiele Diagram
 def McabeThiele(xa, xd, xb, R, q, Pa, Pb, nm):
+  """
+  This is the main function that generates the McCabe-Thiele diagram and displays all the relevant information.
+  It calls the other functions (courbeEQ, DOZE, DOZA, DA, and theoTrays) to plot the equilibrium curve, operating lines, q-line, and step off the trays.
+  Parameters:
+    xa: Mole fraction of the more volatile component in the feed.
+    xd: Mole fraction of the more volatile component in the distillate.
+    xb: Mole fraction of the more volatile component in the bottoms.
+    R: Reflux ratio.
+    q: Thermal condition of the feed.
+    Pa: Saturation vapor pressure of the more volatile component.
+    Pb: Saturation vapor pressure of the less volatile component.
+    nm: Murphy efficiency.
+  """
+  if(q == 1):
+    q -= 0.0000001
+  elif( q == 0):
+    q += 0.0000001 
+
+  #Calling functions
   courbeEQ(Pa, Pb, nm)
   DOZE(xa, xd, R, q)
   DOZA(xa, xd, xb, R, q)
@@ -103,6 +158,7 @@ def McabeThiele(xa, xd, xb, R, q, Pa, Pb, nm):
 
   plt.xlabel('x (Mole fraction of component A in liquid)')
   plt.ylabel('y (Mole fraction of component A in vapor)')
+  plt.title("McCabe-Thiele Diagram [versbyte]")
   plt.xlim(0,1)
   plt.ylim(0,1)
   plt.grid()
@@ -114,8 +170,8 @@ xf = 0.458 # Feed composition
 xd = 0.965 # Distillate composition
 xb = 0.011  # Bottoms composition
 R = 1.4  # Reflux ratio
-q = 0.99 # Feed thermal condition
-Pa = 127 # Saturation vapor pressure of the most volatile component (mmHg)
+q = 1 # Feed thermal condition
+Pa = 95 # Saturation vapor pressure of the most volatile component (mmHg)
 Pb = 23.76 # Saturation vapor pressure of the less volatile component (mmHg)
-nm = 0.80 #murphy efficieny (60-70)%
+nm = 0.75 #murphy efficieny (60-70)%
 McabeThiele(xf, xd, xb, R, q, Pa, Pb, nm)
